42b978d6c5fd65734a001d96269b10a265cdb89c
Dhi-Raj-S <sdhiraj95@gmail.com>
2023-03-07 22:05:53 +0530

    Initial commit

---
+++ src/chatting/application/Server.java
@@ -0,0 +1,16 @@
+package chatting.application;
+
+
+import javax.swing.*;
+
+public class Server extends JFrame {
+
+    Server() {
+
+    }
+
+    public static void main() {
+
+        new Server();
+    }
+}
